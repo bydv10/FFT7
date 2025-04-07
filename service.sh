08eef09122c82017bdc73ca1f1cdd26341473914
MRX7014 | Octopus <safen894@gmail.com>
2025-04-07 11:54:24 +0200

    Delete "#" (AI DETECTED)

--- service.sh
+++ service.sh
@@ -1,9 +1,7 @@
 #!/system/bin/sh
 
-# الانتظار حتى يكتمل إقلاع النظام
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 2
 done
 
-# إبقاء النظام في وضع القراءة/الكتابة بعد كل إقلاع
 mount -o rw,remount /system
