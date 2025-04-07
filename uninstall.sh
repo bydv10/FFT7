931b1bddc9a7ec1ed1f8eaa662be321278bcb634
MRX7014 | Octopus <safen894@gmail.com>
2025-04-07 11:59:10 +0200

    Using EN instead of AR

--- uninstall.sh
+++ uninstall.sh
@@ -1,27 +1,27 @@
 #!/system/bin/sh
 
-# استعادة الملفات من النسخ الاحتياطي
+# Restore files from backup
 BACKUP_DIR="/data/adb/modules/samsung_lite_remover/backup"
 
-echo "[$(date "+%Y-%m-%d %H:%M:%S")] بدء استعادة ملفات Samsung Lite" > /cache/samsung_lite_restore.log
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] Start Restoring Samsung Lite Files" > /cache/samsung_lite_restore.log
 
 if [ -d "$BACKUP_DIR" ]; then
-    # استعادة الملفات
+    # Restoring Files
     for FILE in $BACKUP_DIR/*; do
         FILENAME=$(basename $FILE)
         TARGET_PATH=$(grep -l "$FILENAME" /system -r | head -n 1)
         
         if [ ! -z "$TARGET_PATH" ]; then
             cp -p $FILE $TARGET_PATH
-            echo "[√] تم استعادة: $TARGET_PATH" >> /cache/samsung_lite_restore.log
+            echo "[√] Restored Successfully: $TARGET_PATH" >> /cache/samsung_lite_restore.log
         fi
     done
 else
-    echo "[!] لا يوجد نسخ احتياطية للاستعادة" >> /cache/samsung_lite_restore.log
+    echo "[!] There is no Backup to restore" >> /cache/samsung_lite_restore.log
 fi
 
-# حذف مجلد النسخ الاحتياطي
+# Delete Backup dir
 rm -rf $BACKUP_DIR
 
-# حذف سكربت الحفاظ على وضع RW
+# Delete Force System R/W script
 rm -f /data/adb/service.d/keep_system_rw.sh
